e71afc659ff702798824d9e658dca5d85ee949e0
Showrav Hasan <56910712+showrav715@users.noreply.github.com>
2024-06-05 14:35:35 +0600

    start solveing

--- project/resources/views/admin/dashboard.blade.php
+++ project/resources/views/admin/dashboard.blade.php
@@ -87,7 +87,7 @@
 
     <div class="row">
 
-        <div class="col-12 col-md-6 col-lg-6">
+        <div class="col-12 col-md-12 col-lg-12 col-xl-6">
             <div class="card">
                 <div class="card-header">
                     <h4>
