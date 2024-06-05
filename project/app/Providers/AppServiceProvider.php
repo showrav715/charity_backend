183eab182f45ae36ff7ec4a100e28c169db104a1
Showrav Hasan <56910712+showrav715@users.noreply.github.com>
2024-06-05 12:42:30 +0600

    start

--- project/app/Providers/AppServiceProvider.php
+++ project/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Generalsetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,10 +14,15 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
 
-        if (!file_exists('project/storage/installed') && !request()->is('install') && !request()->is('install/*')) {
-            header("Location: install/");
-            exit;
-        }
+        // if (!file_exists('project/storage/installed') && !request()->is('install') && !request()->is('install/*')) {
+        //     header("Location: install/");
+        //     exit;
+        // }
+
+        
+        view()->composer('*', function ($settings) {
+            $settings->with('gs', Generalsetting::first());
+        });
     }
 
     /**
